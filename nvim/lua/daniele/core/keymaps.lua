d3fc9da8b11b46df6ca6bf71a1f6f1b636dd1b61
danielefedeli <daniele.fedeli@infinitaslearning.com>
2025-01-08 10:15:01 +0100

    Add window navigation with Ctrl + hjkl keys
    Map Ctrl + hjkl for more intuitive split window navigation

--- nvim/lua/daniele/core/keymaps.lua
+++ nvim/lua/daniele/core/keymaps.lua
@@ -24,3 +24,8 @@ keymap.set('n', '[t', '<CMD>BufferLineCyclePrev<CR>')
 -- Map <Esc> in terminal mode to exit to normal mode
 keymap.set('t', '<Esc>', [[<C-\><C-n>]], { noremap = true, silent = true })
 
+-- Remap window navigation
+keymap.set('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+keymap.set('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
+keymap.set('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+keymap.set('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
