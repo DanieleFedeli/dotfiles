768b15b338dc329757bca9201b43c7608df1fc71
danielefedeli <daniele.fedeli@infinitaslearning.com>
2025-03-30 18:58:48 +0200

    Add smart-splits.nvim for unified window navigation
    Enable seamless navigation between neovim splits and wezterm panes

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -15,3 +15,9 @@ vim.keymap.set("n", "<C-a>", "gg<S-v>G")
 -- Use system clipboard
 vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', opts)
 vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', opts)
+
+-- Move between splits
+vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
+vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
+vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
+vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
--- wezterm/keybindings.lua
+++ wezterm/keybindings.lua
@@ -4,19 +4,51 @@ local act = wezterm.action
 
 local Keybindings = {}
 
+local function is_vim(pane)
+	return pane:get_user_vars().IS_NVIM == "true"
+end
+
+local direction_keys = {
+	h = "Left",
+	j = "Down",
+	k = "Up",
+	l = "Right",
+}
+
+local function split_nav(resize_or_move, key)
+	return {
+		key = key,
+		mods = resize_or_move == "resize" and "META" or "CTRL",
+		action = wezterm.action_callback(function(win, pane)
+			if is_vim(pane) then
+				-- pass the keys through to vim/nvim
+				win:perform_action({
+					SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
+				}, pane)
+			else
+				if resize_or_move == "resize" then
+					win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+				else
+					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+				end
+			end
+		end),
+	}
+end
+
 function Keybindings.setup(config)
 	config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 2000 }
 
 	config.keys = {
-		{ key = "h", mods = "CTRL", action = act.ActivatePaneDirection("Left") },
-		{ key = "j", mods = "CTRL", action = act.ActivatePaneDirection("Down") },
-		{ key = "k", mods = "CTRL", action = act.ActivatePaneDirection("Up") },
-		{ key = "l", mods = "CTRL", action = act.ActivatePaneDirection("Right") },
+		split_nav("move", "h"),
+		split_nav("move", "j"),
+		split_nav("move", "k"),
+		split_nav("move", "l"),
 		{ key = "-", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
 		{ key = "\\", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
 		{ key = "[", mods = "LEADER", action = act.ActivateCopyMode },
-		{ key = "Z", mods = "LEADER", action = act.TogglePaneZoomState },
-		{ key = "K", mods = "LEADER", action = act.ClearScrollback("ScrollbackAndViewport") },
+		{ key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
+		{ key = "k", mods = "LEADER", action = act.ClearScrollback("ScrollbackAndViewport") },
 		{
 			key = ",",
 			mods = "LEADER",
