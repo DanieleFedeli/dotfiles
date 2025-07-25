7aacbd5fbf17bb5c0331f62b8c07fad3a1ab0fb5
danielefedeli <daniele.fedeli@infinitaslearning.com>
2025-07-25 21:59:14 +0200

    Switch config from LazyVim to AstroNvim v5 template
    Replace LazyVim setup and plugins with AstroNvim structure.
    Add AstroNvim plugin specs, Mason, Treesitter, and user config examples.
    Remove old LazyVim files and update documentation for AstroNvim usage.

--- nvim/lua/config/options.lua
+++
@@ -1,5 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional optionsoptions here
-vim.opt.clipboard = "" -- disables unnamedplus, so only explicit use of + or * uses the system clipboard
-vim.g.lazyvim_picker = "fzf"
