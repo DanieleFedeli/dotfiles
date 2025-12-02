be279ec0063e709d2ff8e75b149d246340d3bacc
Daniele Fedeli <daniele.fedeli@infinitaslearning.com>
2025-12-02 18:57:48 +0100

    Clean up Neovim config and activate polish.lua with useful autocommands
    - Activate polish.lua with practical autocommands (cursor restore, yank highlight, auto-save, easy close, trim whitespace)
    - Remove placeholder filetype examples from astrocore.lua
    - Remove example LaTeX autopair rules from user.lua

--- nvim/lua/polish.lua
+++ nvim/lua/polish.lua
@@ -1,5 +1,71 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+
+-- Autocommands
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+-- Restore cursor position when opening files
+augroup("RestoreCursor", { clear = true })
+autocmd("BufReadPost", {
+  group = "RestoreCursor",
+  pattern = "*",
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
+-- Highlight on yank
+augroup("YankHighlight", { clear = true })
+autocmd("TextYankPost", {
+  group = "YankHighlight",
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
+  end,
+})
+
+-- Auto-save when leaving insert mode or text changes
+augroup("AutoSave", { clear = true })
+autocmd({ "InsertLeave", "TextChanged" }, {
+  group = "AutoSave",
+  pattern = "*",
+  callback = function()
+    if vim.bo.modified and not vim.bo.readonly and vim.fn.expand("%") ~= "" and vim.bo.buftype == "" then
+      vim.api.nvim_command("silent update")
+    end
+  end,
+})
+
+-- Close certain filetypes with 'q'
+augroup("EasyClose", { clear = true })
+autocmd("FileType", {
+  group = "EasyClose",
+  pattern = { "help", "man", "qf", "lspinfo", "checkhealth" },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
+
+-- Remove trailing whitespace on save
+augroup("TrimWhitespace", { clear = true })
+autocmd("BufWritePre", {
+  group = "TrimWhitespace",
+  pattern = "*",
+  callback = function()
+    local save_cursor = vim.fn.getpos(".")
+    vim.cmd([[%s/\s\+$//e]])
+    vim.fn.setpos(".", save_cursor)
+  end,
+})
+
+-- Custom functions
+vim.api.nvim_create_user_command("FormatJson", function()
+  vim.cmd([[%!jq .]])
+end, { desc = "Format JSON using jq" })
