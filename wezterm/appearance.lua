b784df610328539544c6da1829b396f3e9549914
Daniele Fedeli <daniele.fedeli@infinitaslearning.com>
2025-12-02 19:20:56 +0100

    Add simple status bar to WezTerm
    - Show workspace/key table indicator on left
    - Show current directory and time on right
    - Cleaner implementation than previous version

--- wezterm/appearance.lua
+++ wezterm/appearance.lua
@@ -20,4 +20,54 @@ function Appearance.setup(config)
 	config.hide_tab_bar_if_only_one_tab = false
 end
 
+-- Simple status bar
+wezterm.on("update-status", function(window, pane)
+	local stat = window:active_workspace()
+	local stat_color = "#f7768e"
+	
+	-- Show active key table if any
+	if window:active_key_table() then
+		stat = window:active_key_table()
+		stat_color = "#7dcfff"
+	end
+	if window:leader_is_active() then
+		stat = "LDR"
+		stat_color = "#bb9af7"
+	end
+
+	-- Get current directory
+	local function basename(s)
+		return string.gsub(s, "(.*[/\\])(.*)", "%2")
+	end
+
+	local cwd = pane:get_current_working_dir()
+	if cwd then
+		if type(cwd) == "userdata" then
+			cwd = basename(cwd.file_path)
+		else
+			cwd = basename(cwd)
+		end
+	else
+		cwd = ""
+	end
+
+	local time = wezterm.strftime("%H:%M")
+
+	-- Left status
+	window:set_left_status(wezterm.format({
+		{ Foreground = { Color = stat_color } },
+		{ Text = "  " },
+		{ Text = wezterm.nerdfonts.oct_table .. "  " .. stat },
+		{ Text = " |" },
+	}))
+
+	-- Right status
+	window:set_right_status(wezterm.format({
+		{ Text = wezterm.nerdfonts.md_folder .. "  " .. cwd },
+		{ Text = " | " },
+		{ Text = wezterm.nerdfonts.md_clock .. "  " .. time },
+		{ Text = "  " },
+	}))
+end)
+
 return Appearance
